2f9b3225036c8c64e1f76355a2ce1d451a0e70be
Karla Degrano-Sebandal <karla.degrano@gmail.com>
2023-07-31 13:51:45 +0800

    test

--- server/database.sql
+++ server/database.sql
@@ -3,4 +3,9 @@ CREATE DATABASE perntodo;
 CREATE TABLE todo(
   todo_id SERIAL PRIMARY KEY,
   description VARCHAR(255)
-);
+)
+
+INSERT INTO todo(description) VALUES ('Playing table tennis.')
+INSERT INTO todo(description) VALUES ('Sea kayaking with friends.')
+
+SELECT * FROM todo
--- server/index.js
+++ server/index.js
@@ -81,9 +81,6 @@ app.delete("/todos/:id", async(req, res) => {
     }
 })
 
-
-
-
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
 });
